80c423600e6f47645a6fcca679ac3e20392fe2df
Adam The Canadian <adam.jarrett.clare@gmail.com>
2019-06-09 11:46:17 -0600

    Add possible moves coloring
    Added coloring of the possible moves from the starting location. This is
    hardcoded for now, and will be broken down into a separate function
    later.

--- week1/grid_generator.py
+++ week1/grid_generator.py
@@ -2,16 +2,22 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
 
-rows = 100
-cols = 100
+rows = 10
+cols = 10
 data = np.zeros(rows * cols).reshape(rows, cols)
+# Hard coding the start, goal, and obstacles for now.
 data[1, 1] = 2
 data[9, 9] = 3
 data[4, 4] = 1
 data[5, 5] = 1
+# Possible moves
+data[0, 1] = 5
+data[1, 0] = 5
+data[1, 2] = 5
+data[2, 1] = 5
 # create discrete colormap
-cmap = colors.ListedColormap(['white', 'black', 'green', 'red'])
-bounds = [0,1,2,3,4]
+cmap = colors.ListedColormap(['white', 'black', 'green', 'red', 'blue'])
+bounds = [0,1,2,3,4,5]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
 fig, ax = plt.subplots()
@@ -22,6 +28,6 @@ ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=1)
 ax.set_xticks(np.arange(0.5, rows, 1));
 ax.set_yticks(np.arange(0.5, cols, 1));
 plt.tick_params(axis='both', labelsize=0, length = 0)
-fig.set_size_inches((8.5, 11), forward=False)
+# fig.set_size_inches((8.5, 11), forward=False)
 plt.savefig('images/grid.png', dpi=500)
 plt.show()
