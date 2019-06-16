b1e6d1d2fc4be56a5e6fc81fec71d88603ea4e4c
Adam Clare <adam.jarrett.clare@gmail.com>
2019-06-16 12:21:32 -0600

    Remove tick labels

--- week1/grid_generator.py
+++ week1/grid_generator.py
@@ -21,7 +21,7 @@ def plot_grid(data, saveImageName):
     ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=1)
     ax.set_xticks(np.arange(0.5, rows, 1));
     ax.set_yticks(np.arange(0.5, cols, 1));
-    plt.tick_params(axis='both', labelsize=0, length = 0)
+    plt.tick_params(axis='both', which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
     # fig.set_size_inches((8.5, 11), forward=False)
     plt.savefig(saveImageName + ".png", dpi=500)
 
