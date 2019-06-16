6526683f8cadda788d9b1c350dd82415fd6e5957
Adam Clare <adam.jarrett.clare@gmail.com>
2019-06-16 14:53:45 -0600

    Add title and labels

--- week1/bfs_complexity.py
+++ week1/bfs_complexity.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
 
-# BFS == Bread First Search.
+# BFS == Breadth First Search.
 # This script plots the O notation (complexity) of using BFS
 # to search a grid of N cells. In this example we are looking at 
 # cells of 1, 2, 3, ... all the way to 1000 cells.
@@ -26,7 +26,12 @@ for num_dimension in range(min_dimensions, max_dimensions + 1):
     plt.plot(number_of_cells_in_one_dimension, complexity, label=legend_label)
     
 plt.legend()
+plt.title("Breadth-First Search Complexity Zoomed Out")
+plt.xlabel("Number of Cells in One Dimension")
+plt.ylabel("Complexty O(|V|)")
 plt.savefig("images/bfs_zoomed_out.png", dpi=500)
 
+plt.title("Breadth-First Search Complexity Zoomed In")
 plt.ylim([0, 1e6])
+plt.ylabel("Complexty O(|V|)")
 plt.savefig("images/bfs_zoomed_in.png", dpi=500)
