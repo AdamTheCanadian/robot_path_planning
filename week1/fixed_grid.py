9c0d0b0c785ae3cf7c57d78413d9ff62a4c7828c
Adam Clare <adam.jarrett.clare@gmail.com>
2019-06-22 16:57:20 -0600

    Move goal position to make sure search stops at correct time

--- week1/fixed_grid.py
+++ week1/fixed_grid.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     data[1, 1] = grid_generator.START_CELL
     data[9, 9] = grid_generator.GOAL_CELL
     grid_generator.generate_obstacles(data, 5)
-    grid_generator.plot_grid(data, "fixed_grid/fixed_grid")
+    # grid_generator.plot_grid(data, "fixed_grid/fixed_grid")
+    grid = np.loadtxt('fixed_grid/fixed_grid.txt', dtype=int)
+    grid_generator.plot_grid(grid, 'fixed_grid/fixed_grid')
     # This line was puroposely commented out so I dont accidently 
     # overwrite my truth file. Uncomment if wanting to generate new
     # truth grid
